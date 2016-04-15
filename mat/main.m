a288be9eb3d55498d6f50123900ffc8bcc0dfdf2
Phil Chodrow <pchodrow@mit.edu>
2016-04-15 15:45:17 -0400

    small mods

--- mat/main.m
+++ mat/main.m
@@ -1,13 +1,18 @@
+cd('/Users/phil/Google Drive/classes/6.437J/project/mat')
+
+
 load('example_cipher.mat')      % Load example
 load('language_parameters.mat') % Load parameters
 
-sub = ciphertext(1:500);
+sub = ciphertext(1:3000);
+
+c1 = warm_start(ciphertext, letter_probabilities, alphabet);
 
 cipher_function = cipher_function(randperm(length(cipher_function)))
-
 c1 = cipher(alphabet, cipher_function);
 
-for i = 1:5000
+
+for i = 1:10000
     switch1 = randi([1,length(alphabet)]);
     switch2 = randi([1,length(alphabet)]);
     while switch1 == switch2
@@ -19,19 +24,20 @@ for i = 1:5000
     if a<t
         c1 = containers.Map(keys(c2), values(c2));
     end
+    disp(decipher(c1, sub))
 end
-
-for i = 1:2000
-    switch1 = randi([1,length(alphabet)]);
-    switch2 = randi([1,length(alphabet)]);
-    while switch1 == switch2
-        switch2 = randi([1,length(alphabet)]);
-    end
-    c2 = permute_cipher(c1, alphabet(switch1), alphabet(switch2));
-    t = compare_ciphers(c1, c2, ciphertext, alphabet, letter_probabilities, letter_transition_matrix);
-    a = unifrnd(0,1);
-    if a<t
-        c1 = containers.Map(keys(c2), values(c2));
-    end
-end
+% 
+% for i = 1:2000
+%     switch1 = randi([1,length(alphabet)]);
+%     switch2 = randi([1,length(alphabet)]);
+%     while switch1 == switch2
+%         switch2 = randi([1,length(alphabet)]);
+%     end
+%     c2 = permute_cipher(c1, alphabet(switch1), alphabet(switch2));
+%     t = compare_ciphers(c1, c2, ciphertext, alphabet, letter_probabilities, letter_transition_matrix);
+%     a = unifrnd(0,1);
+%     if a<t
+%         c1 = containers.Map(keys(c2), values(c2));
+%     end
+% end
 
--- mat/test_cipher.m
+++ mat/test_cipher.m
@@ -1,16 +1,16 @@
 function likelihood = test_cipher(cipher, ciphertext, alphabet, letter_p, letter_t)
-    plaintext = decipher(cipher, ciphertext)
+    plaintext = decipher(cipher, ciphertext);
     likelihood = compute_likelihood(plaintext, alphabet, letter_p, letter_t);
 end
 
 
-function likelihood = compute_likelihood(plaintext, alphabet, letter_p, letter_t)
+function log_likelihood = compute_likelihood(plaintext, alphabet, letter_p, letter_t)
     p = log(letter_p(alphabet == plaintext(1)));
     for i = 2:length(plaintext)
         j = alphabet == plaintext(i-1);
         k = alphabet == plaintext(i);
         p = p + log(letter_t(k, j));
     end
-    likelihood = p;
+    log_likelihood = p;
 end
 
